0fd252b3582c3a8baf8ff4716a04ff8c693debc5
Feoktist92 <feoktist92@gmail.com>
2023-07-27 20:36:49 +0800

    header changes

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-React food project made with API <a href='https://www.themealdb.com' target="__blank">TheMealDB</a>
+React SPA food project made with API <a href='https://www.themealdb.com' target="__blank">TheMealDB</a>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,25 +1,20 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
     return (
         <nav className='orange lighten-4'>
             <div className='nav-wrapper'>
-                <Link to='/' className='brand-logo brown-text'>
+                <NavLink to='/' className='brand-logo brown-text center'>
                     React Shop
-                </Link>
-                <ul id='nav-mobile' className='right hide-on-med-and-down'>
-                    <li>
-                        <Link to='/about' className='brown-text'>
-                            {' '}
-                            About
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='/contacts' className='brown-text'>
-                            Contacts
-                        </Link>
-                    </li>
-                </ul>
+                </NavLink>
+
+                <NavLink to='/about' className='brown-text right '>
+                    About
+                </NavLink>
+
+                <NavLink to='/contacts' className='brown-text right contacts'>
+                    Contacts
+                </NavLink>
             </div>
         </nav>
     );
